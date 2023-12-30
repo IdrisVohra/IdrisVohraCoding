1e25b3bb4b7d7c3fd24085bcaa0f1a8d208ca709
Idris Vohra <idrees786.iv@gmail.com>
2023-12-30 23:30:10 +0530

    Adding Sales Suggestion Code

--- SSE_NodeJs/src/index.js
+++ SSE_NodeJs/src/index.js
@@ -4,14 +4,23 @@ const hbs = require("hbs")
 const path = require("path");
 const templatePath=path.join(__dirname,'../templates');
 const database = require('../database/databasQuery');
+const images=path.join(__dirname,'../images');
 
 const connectDb = require('../src/dbConnection');
 connectDb();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(express.static(images));
+
 app.set("view engine","hbs");
 app.set("views",templatePath)
+
+app.get("/dynamic", (req, res) => { 
+    imageList = [];
+    imageList.push({ src: "/images/main_logo.png", name: "javascript" });
+    res.render("dynamic", { imageList: imageList }); 
+});
 
 app.get("/",(req,res)=>{
     res.render("login");
@@ -26,7 +35,7 @@ app.post("/signup",async (req,res)=>{
         name:req.body.name,
         paswword:req.body.paswword
     }
-    database.InsertData();
+    //database.InsertData();
     res.render("home");
 });
 app.listen(3000,()=>{
