b8e22f7a48b2ad6588eeb288e3e06de117ee1c6e
i.muffaddal.vohra <i.muffaddal.vohra@accenture.com>
2024-01-03 13:07:52 +0530

    Making Changes to use it generally

--- SSE_NodeJs/src/dbConnection.js
+++ SSE_NodeJs/src/dbConnection.js
@@ -12,6 +12,19 @@ const connectDb = async ()=>{
                 enableArithAbort: true,
                 instancename: 'MSSQLSERVER'
             },
+            /* Added when using in Accenture laptop
+            const connect={  
+                user :'DC_UAT_V8_MIG',
+                password :'92uWaD4YH_Vwtg3`',
+                server:'10.149.0.71',
+                database:'DMS_VNM_QA',
+                schema:'dbo',
+                multipleStatements: true,
+                options:{
+                    trustedconnection: true,
+                    enableArithAbort : true,  
+                    instancename :'MSSQLSERVER'
+                },*/
             port: 1433
         };
             console.log("Database Connected");
