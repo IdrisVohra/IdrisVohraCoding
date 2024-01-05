8b366dc62261b67bb11ec99bda5851b027532a12
Idris Vohra <idrees786.iv@gmail.com>
2024-01-05 16:49:39 +0530

    code changes

--- BackEndNodeJs/express-project/mycontacts-backend/server.js
+++ BackEndNodeJs/express-project/mycontacts-backend/server.js
@@ -9,6 +9,8 @@ const app = express()
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
